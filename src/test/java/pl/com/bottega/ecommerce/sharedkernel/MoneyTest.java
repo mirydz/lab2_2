b6f2d8607e6fde2bb07e135dce3dde285867c82e
Michal Rydz <mirydz@gmail.com>
2015-04-21 07:56:01 +0200

    Add test for adding two Money objects with the same currency

---
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -0,0 +1,20 @@
+package pl.com.bottega.ecommerce.sharedkernel;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MoneyTest {
+
+    @Test
+    public void shouldAddMoneyOfTheSameCurrency() {
+        Money money = new Money(22.33, "USD");
+        Money moneyToAdd = new Money(11.22, "USD");
+
+        Money result = money.add(moneyToAdd);
+
+        Money expected = new Money(33.55, "USD");
+        assertThat(result, is(expected));
+    }
+}
