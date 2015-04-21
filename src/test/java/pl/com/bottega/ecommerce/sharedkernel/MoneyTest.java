41f31141f79ee03b22680f77077c2b3c44d6fba5
Michal Rydz <mirydz@gmail.com>
2015-04-21 08:40:10 +0200

    add test for Money multiplication by value

--- src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sharedkernel/MoneyTest.java
@@ -2,6 +2,8 @@ package pl.com.bottega.ecommerce.sharedkernel;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -45,5 +47,18 @@ public class MoneyTest {
         money.subtract(moneyToSubtract);
     }
 
+    @Test
+    public void ShouldMultiplyMoneyByValue() {
+        Money money = new Money(11.11, "GBP");
+        BigDecimal multiplierBD = new BigDecimal("2.0");
+        Double multiplierDouble = 2.00;
+
+        Money resultBD = money.multiplyBy(multiplierBD);
+        Money resultDouble = money.multiplyBy(multiplierDouble);
+
+        Money expected = new Money(22.22, "GBP");
+        assertThat(resultBD, is(expected));
+        assertThat(resultDouble, is(expected));
+    }
 
 }
